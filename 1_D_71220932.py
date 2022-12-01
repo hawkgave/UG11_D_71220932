ca41d58b0318c9e018b7539e0b41b4111af601d5
hawkgave <117901120+hawkgave@users.noreply.github.com>
2022-12-01 17:04:25 +0700

    Add files via upload

--- 1_D_71220932.py
+++ 1_D_71220932.py
@@ -1,16 +1,13 @@
 print("Halo selamat datang di bioskop!")
 def lokasi():
-    1=XII Empire
-    2=XII Amplaz
-    3=XII JCM
     print("1) XXI Empire")
     print("2) XXI Amplaz")
     print("3) XXI JCM")
     milih=int(input("Masukkan pilihanmu:"))
-        if milih<3:
-            print("Pilihan tidak tersedia")
-        else:
-            lokasi()
+    if(milih<3):
+        print("Pilihan tidak tersedia")
+    exit()
+        
 
 def judulfilm():
     print("Mau nonton film apa nih? Ada film:")
@@ -18,10 +15,9 @@ def judulfilm():
     print("2. Keramat")
     print("3. KKN di Desa Penari")
     pilihan=int(input("Pilih nomer film: "))
-        if pilihan>=3:
-            print("Pilihan tidak tersedia")
-        else:
-            judulfilm()
+    if (pilihan>=3):
+        print("Pilihan tidak tersedia")
+    exit()
 
 def bioskop():
     print("Mau nonton layar bioskop apa:")
@@ -29,10 +25,9 @@ def bioskop():
     print("2. Dolby Almos")
     print("3. Premier")
     milihlah=int(input("Pilih nomor tipe layar: "))
-        if milihlah>=3:
-            print("Pilihan tidak tersedia")
-        else:
-            bioskop()
+    if(milihlah>=3):
+        print("Pilihan tidak tersedia")
+    exit()
 
 print("Berapa banyak tiket?")
 
@@ -43,10 +38,6 @@ def jamNonton():
     print("3. 16.55")
     print("4. 19.05")
     pilihlagi=int(input("Masukkan angka pilihan anda:"))
-    if pilihlagi>=3:
+    if(pilihlagi>=3):
         print("Pilihan tidak tersedia")
-    else:
-        jamNonton()
-
-if(milih,pilihan,milihlah,pilihlagi<=3)
-    print("oke berhasil!, silahkan dinikmati di jam")
+    exit()
